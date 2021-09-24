53f515f4712e1b660acd6e3602f736b3955315d0
Lincoln-LM <maestaslincoln1@gmail.com>
2021-09-23 23:49:00 -0600

    readar radar reader

--- src/pkrd/rng/gen6.rs
+++ src/pkrd/rng/gen6.rs
@@ -32,6 +32,77 @@ impl Gen6Rng {
         self.mt_state
     }
 
+    pub fn get_patches(&mut self) -> [[u8;9];9] {
+        let mut patches = [[5;9];9];
+        patches[4][4] = 4;
+        let mut go = self.tinymt_rng;
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+        go.next_state();
+
+        go.next_state();
+        let good_rate = [23,43,63,83];
+        let mut ring = 0u64;
+        while ring < 4 {
+            let mut state = 0;
+            let mut direction = ((go.next() as u64 * 4u64) >> 32);
+            let mut location = ((go.next() as u64 * (ring as u64 * 2u64 + 3u64)) >> 32);
+            if ((go.next() as u64 * 100) >> 32) < good_rate[ring as usize] {
+                state = 1;
+                go.next();
+                go.next();
+            }
+            patches = self.set_patch(patches,ring as u32,direction as u32,location as u32,state);
+            ring += 1;
+        }
+        ring = ((go.next() as u64 * 3u64) >> 32);
+        let mut direction = ((go.next() as u64 * 4u64) >> 32);
+        let mut location = ((go.next() as u64 * (ring as u64 * 2u64 + 3u64)) >> 32);
+        let mut state = 3;
+        patches = self.set_patch(patches,ring as u32,direction as u32,location as u32,state);
+        patches
+    }
+
+    pub fn set_patch(&mut self, mut patches: [[u8;9];9], ring: u32, direction: u32, location: u32, state: u8) -> [[u8;9];9] {
+        let mut x = 4;
+        let mut y = 4;
+        if direction == 0 || direction == 1 {
+            x = 3 - ring + location;
+        }
+        else if direction == 2 {
+            x = 3 - ring;
+        }
+        else {
+            x = 5 + ring;
+        }
+        if direction == 0 {
+            y = 3 - ring;
+        }
+        else if direction == 1 {
+            y = 5 + ring;
+        }
+        else {
+            y = 3 - ring + location;
+        }
+        patches[x as usize][y as usize] = state;
+        patches
+    }
+
     pub fn update(&mut self, mt_state: u32, init_seed: u32, tinymt_state: [u32; 4]) {
         if self.init_seed != init_seed && init_seed != 0 {
             self.mt_rng = mt::MT::new(init_seed);
--- src/pkrd/views/rng6.rs
+++ src/pkrd/views/rng6.rs
@@ -1,5 +1,7 @@
 use crate::pkrd::{display, display::Screen, reader, rng};
 use ctr::res::CtrResult;
+use alloc::format;
+use alloc::borrow::ToOwned;
 
 pub fn run_view(
     game: &impl reader::Gen6Reader,
@@ -7,54 +9,64 @@ pub fn run_view(
     screen: &mut display::DirectWriteScreen,
 ) -> CtrResult<()> {
     if screen.get_is_top_screen() {
-        let mut x = 190;
+        let mut x = 10;
         let mut y = 10;
 
         let black = display::Color::black();
         let white = display::Color::white();
 
-        screen.paint_square(&black, x, y, 200, 140)?;
+        screen.paint_square(&black, x, y, 200, 160)?;
 
         x += 10;
         y += 4;
-        screen.draw_string(&white, "Hello from rust!", x, y)?;
+        let types = ["B","G","S","X","C","#"];
+        let patches = rng.get_patches();
+        for row in 0..9 {
+            let mut row_string = "".to_owned();
+            for column in 0..9 {
+                row_string = format!("{} {}", row_string, types[patches[column][row] as usize]).to_owned();
+            }
+            screen.draw_string(&white, &row_string, x, y)?;
+            y += 12;
+        }
+        // screen.draw_string(&white, "Hello from rust!", x, y)?;
 
-        y += 16;
-        let init_seed = game.get_initial_seed()?;
-        let seed_text = &alloc::format!("Init seed: {:08X}", init_seed);
-        screen.draw_string(&white, seed_text, x, y)?;
+        // y += 16;
+        // let init_seed = game.get_initial_seed()?;
+        // let seed_text = &alloc::format!("Init seed: {:08X}", init_seed);
+        // screen.draw_string(&white, seed_text, x, y)?;
 
-        y += 12;
-        let mt_state = game.get_mt_state()?;
-        let state_text = &alloc::format!("Curr state: {:08X}", mt_state);
-        screen.draw_string(&white, state_text, x, y)?;
+        // y += 12;
+        // let mt_state = game.get_mt_state()?;
+        // let state_text = &alloc::format!("Curr state: {:08X}", mt_state);
+        // screen.draw_string(&white, state_text, x, y)?;
 
-        y += 12;
-        let mt_advances = rng.get_mt_advances();
-        let advances_text = &alloc::format!("Advances: {}", mt_advances);
-        screen.draw_string(&white, advances_text, x, y)?;
+        // y += 12;
+        // let mt_advances = rng.get_mt_advances();
+        // let advances_text = &alloc::format!("Advances: {}", mt_advances);
+        // screen.draw_string(&white, advances_text, x, y)?;
 
-        y += 12;
-        let index = rng.get_tinymt_advances();
-        let index_text = &alloc::format!("Index: {}", index);
-        screen.draw_string(&white, index_text, x, y)?;
+        // y += 12;
+        // let index = rng.get_tinymt_advances();
+        // let index_text = &alloc::format!("Index: {}", index);
+        // screen.draw_string(&white, index_text, x, y)?;
 
-        // should be moved to its own view?
-        y += 12;
-        screen.draw_string(&white, "Initial state:", x, y)?;
+        // // should be moved to its own view?
+        // y += 12;
+        // screen.draw_string(&white, "Initial state:", x, y)?;
 
-        y += 12;
-        let initial_state = rng.get_initial_tinymt_state();
-        let tinymt_text =
-            &alloc::format!("[3]{:08X} [2]{:08X}", initial_state[3], initial_state[2]);
-        screen.draw_string(&white, tinymt_text, x, y)?;
+        // y += 12;
+        // let initial_state = rng.get_initial_tinymt_state();
+        // let tinymt_text =
+        //     &alloc::format!("[3]{:08X} [2]{:08X}", initial_state[3], initial_state[2]);
+        // screen.draw_string(&white, tinymt_text, x, y)?;
 
-        y += 12;
-        let tinymt_text =
-            &alloc::format!("[1]{:08X} [0]{:08X}", initial_state[1], initial_state[0]);
-        screen.draw_string(&white, tinymt_text, x, y)?;
+        // y += 12;
+        // let tinymt_text =
+        //     &alloc::format!("[1]{:08X} [0]{:08X}", initial_state[1], initial_state[0]);
+        // screen.draw_string(&white, tinymt_text, x, y)?;
 
-        y += 12;
+        // y += 12;
         screen.draw_string(&white, "Current state:", x, y)?;
 
         y += 12;
